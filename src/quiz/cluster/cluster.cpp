96efbb8ecc3b6cb526ee251822eeec0a72464dbc
Yogesh Gajjar <yogeshgajjar75@gmail.com>
2020-12-27 03:02:39 +0000

    eucledian clustering

--- src/quiz/cluster/cluster.cpp
+++ src/quiz/cluster/cluster.cpp
@@ -75,12 +75,38 @@ void render2DTree(Node* node, pcl::visualization::PCLVisualizer::Ptr& viewer, Bo
 
 }
 
+void clusterTemp(int id, std::vector<int> &cluster, const std::vector<std::vector<float>>& points, float distanceTol, KdTree* tree, std::vector<bool> points_processed) {
+
+	points_processed[id] = true;
+
+	cluster.push_back(id);
+	std::vector<int> clust_id = tree->search(points[id], distanceTol);
+
+	for(const auto &i:clust_id) {
+		if(!points_processed[i]) {
+			clusterTemp(i, cluster, points, distanceTol, tree, points_processed);
+		}
+	}
+}
+
 std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol)
 {
 
 	// TODO: Fill out this function to return list of indices for each cluster
 
 	std::vector<std::vector<int>> clusters;
+	std::vector<bool> points_processed(points.size(), false);
+
+	for(int i=0; i < points.size(); i++) {
+		if(points_processed[i])
+			continue;
+
+		
+		std::vector<int> cluster;
+		clusterTemp(i, cluster, points, distanceTol, tree, points_processed);
+		clusters.push_back(cluster);
+		
+	}
  
 	return clusters;
 
--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -28,18 +28,14 @@ struct KdTree
 	{}
 
 	void insertBST(Node **node, int depth, vector<float> point, int id) {
-		// cout << "Entered helper function" << endl;
 
 		if(*node == NULL) {
-			// cout << "in null" << endl;
 			*node = new Node(point, id);
 		}
 
 		else {
-			int kd_depth = depth % 2;
-			// cout << kd_depth << endl;
-
-			if(point[kd_depth] < (*node)->point[kd_depth])
+			
+			if(point[depth % 2] < (*node)->point[depth % 2])
 				insertBST(&((*node)->left), depth+1, point, id);
 			else 
 				insertBST(&((*node)->right), depth+1, point, id);
@@ -50,14 +46,33 @@ struct KdTree
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
-
 		insertBST(&root, 0, point, id);
 	}
 
+	void searchBST(vector<float> &target, vector<int> &ids, float distanceTol, Node *node, int depth) {
+
+		// target(x1,y1), node->points(x2,y2) -> calculate eucledian distance
+		if(node) {
+			// if the point is within the distance tolerance
+
+			if((node->point[0] >= (target[0] - distanceTol) && node->point[0] <= (target[0] + distanceTol)) && (node->point[1] >= (target[1] - distanceTol) && node->point[1] <= (target[1] + distanceTol))) {
+				float dist = sqrt(pow((node->point[0] - target[0]),2) + pow((node->point[1] - target[1]),2));
+				if(dist <= distanceTol)
+					ids.push_back(node->id);
+			}
+
+			//else 
+			if((target[depth % 2]-distanceTol) < node->point[depth % 2]) 
+				searchBST(target, ids, distanceTol, node->left, depth+1);
+			if((target[depth % 2]+distanceTol) > node->point[depth % 2]) 
+				searchBST(target, ids, distanceTol, node->right, depth+1);
+		}
+	}
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		searchBST(target, ids, distanceTol, root, 0);
 		return ids;
 	}
 	
