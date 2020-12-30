35fbd960a58e826711d57a18fc219e982a6064c7
Yogesh Gajjar <yogeshgajjar75@gmail.com>
2020-12-30 23:12:06 +0000

    changes to kdtree.h

--- src/environment.cpp
+++ src/environment.cpp
@@ -92,8 +92,8 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointCloud
     	tree->insert(pair_cloud.first->points[i],i);
 
     // std::vector<pcl::PointXYZI> clusters;
-    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(pair_cloud.first, 0.5, 50, 500);
-    // std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->myeuclideanCluster(pair_cloud.first, tree, 0.5, 50, 500);
+    // std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(pair_cloud.first, 0.5, 50, 500);
+    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->myeuclideanCluster(pair_cloud.first, tree, 0.5, 50, 500);
 
     renderPointCloud(viewer, pair_cloud.first, "obstacle_cloud", Color(1,0,0));
     renderPointCloud(viewer, pair_cloud.second, "obstacle_road", Color(0,1,0));
